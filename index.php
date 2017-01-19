240e7e3b2ee163d8ed0558bd396fe171d8aa9528
Cesvenbl <cedrik.svenblad@gymnasium.ax>
2017-01-19 15:47:55 +0200

    Broken piece of shit
    it’s broken as hell

--- index.php
+++ index.php
@@ -16,9 +16,9 @@
 
 			<div class="text-center">
 
-				<h1>Welcome Senpai</h1>	
-
 				<?php if ($_GET['name'] == NULL): ?>
+
+					<h1>Welcome Senpai</h1>	
 
 					<form>
 						<label for="name">Senpai what is your name?</label>
@@ -26,11 +26,21 @@
 						<input type="submit">
 					</form>
 
-				<?php elseif ($_GET['name'] == Liam || liam || yamada || Yamada):?>
+				<?php 
 
-					<h1><?php print($_GET['name']) ?> Ta dig och dina ideer</h1>
+					else;
+					$new_name = $_GET[]'name';
+					$half_name_length = (int) (mb_strlen($new_name) / 2);
+					$remaining_chars = mb_strlen($new_name) - $half_name_length;
+					$name_end = mb_substr($new_name, $half_name_length, $remaining_chars);
+					$name_beginning = mb_substr($new_name, 0, $half_name_length);
+					$botname = $name_end . $name_beginning;
 
-				<?php endif ?>
+				?>
+
+					<h1>Welcome <?php print($_GET['name']) ?>-chan. <br> My name is <?php $botname ?></h1>
+
+				<?php endif; ?>
 
 			</div>
 
