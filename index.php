77d088f4dd16e0c41412c9a03d447cc60ad899cb
Cesvenbl <cedrik.svenblad@gymnasium.ax>
2017-01-19 12:46:03 +0200

    form and css
    Basic Form and css styling

--- index.php
+++ index.php
@@ -1,1 +1,22 @@
-<?php  ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
+	<link href="https://fonts.googleapis.com/css?family=VT323" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<title>ChatBot</title>
+</head>
+<body>
+	<div class="col-md-4 offset-md-4 rounded">
+			<div class="text-center">
+				<h1>Welcome Wanker</h1>	
+				<form>
+					<label for="name">Your Name?</label>
+					<input type="text" name="name">
+					<input type="submit">
+				</form>
+			</div>
+	</div>
+</body>
+</html>
