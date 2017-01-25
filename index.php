c10921655ff8b4253d501bf2e79d8348fbb41606
Cesvenbl <cedrik.svenblad@gymnasium.ax>
2017-01-25 13:30:16 +0200

    redone

--- index.php
+++ index.php
@@ -1,53 +1,6 @@
-<!DOCTYPE html>
+<?php
+header("content-type: text/plain; charset-utf-8");
+echo "hej";
+	
 
-<html>
-
-<head>
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
-	<link href="https://fonts.googleapis.com/css?family=VT323" rel="stylesheet">
-	<link rel="stylesheet" type="text/css" href="css/style.css">
-	<title>ChanBot</title>
-</head>
-<?php 
-	$new_name = $_GET['name'];
-	$half_name_length = (mb_strlen($new_name) / 2);
-	$remaining_chars = (mb_strlen($new_name) - $half_name_length);
-	$name_end = mb_substr($new_name, $half_name_length, $remaining_chars);
-	$name_beginning = mb_substr($new_name, 0, $half_name_length);
-	$botname = ($name_end . $name_beginning);
- ?>
-<body>
-
-	<div class="col-md-4 offset-md-4 rounded">
-
-			<div class="text-center">
-
-				<?php if ($_GET['name'] == NULL): ?>
-
-					<h1>Welcome Senpai</h1>	
-
-					<form>
-						<label for="name">Senpai what is your name?</label>
-						<input type="text" name="name">
-						<input type="submit">
-					</form>
-
-
-				<?php elseif ($_GET['name'] == 'liam' || $_GET['name'] == 'Liam' || $_GET['name'] == 'Yamada' || $_GET['name'] == 'yamada'): ?>
-					
-					<h1><?php print($_GET['name']) ?> You are a fucking cunt</h1>
-
-				<?php elseif  ($_GET['name'] != 'liam' || $_GET['name'] != 'Liam' || $_GET['name'] != 'Yamada' || $_GET['name'] != 'yamada'): ?>
-
-					<h1>Welcome <?php print($_GET['name']) ?>-chan. <br> My name is <?php print($botname) ?></h1>
-
-				<?php endif; ?>
-
-			</div>
-
-	</div>
-
-</body>
-
-</html>
+?>
